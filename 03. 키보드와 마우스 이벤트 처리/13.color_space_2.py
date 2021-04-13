d201eb70e4f148c970063817f1c79cfca1e90e02
Amadeus-System <76824867+Amadeus-System@users.noreply.github.com>
2021-04-13 19:43:09 +0200

    Add files via upload

---
+++ 03. 키보드와 마우스 이벤트 처리/13.color_space_2.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+H = cv2.imread('cat-h.png', cv2.IMREAD_GRAYSCALE)
+S = cv2.imread('cat-s.png', cv2.IMREAD_UNCHANGED)
+V = cv2.imread('cat-v.png', cv2.IMREAD_UNCHANGED)
+
+# BGR 처럼 merge할 경우 잘 못된 이미지를 생성한다.
+HSV = cv2.merge((H,S,V))
+BGR = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
+
+cv2.imshow('H', H)
+cv2.imshow('S', S)
+cv2.imshow('V', V)
+cv2.imshow('HSV', HSV)
+cv2.imshow('BGR', BGR)
+cv2.waitKey()
+
+# [Quiz] image/hats_small.png 파일의 명도를 20% 줄여
+# - image/hats_smallDim.png로 저장하자.
